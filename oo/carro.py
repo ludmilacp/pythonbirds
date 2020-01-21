7453f4c6b98a95bc92efb8d4f9d45a8a736c60aa
ludmilacp <ludmila.canfield@gmail.com>
2020-01-21 21:47:47 -0200

    'implementado o Carro'

--- oo/carro.py
+++ oo/carro.py
@@ -1,15 +1,25 @@
-"""
-Criar uma classe carro...
-    Exemplo:
-    >>> # Testando motor
-    >>> motor = Motor()
-    >>> motor.velocidade
-    0
-    >>>motor.acelerar()
-    >>> motor.velocidade
-    1
-    >>>
-"""
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
 
 NORTE='Norte'
 SUL='Sul'
@@ -43,6 +53,3 @@ class Motor:
     def frear(self):
         self.velocidade -=2
         self.velocidade = max(0, self.velocidade)
-
-
-
