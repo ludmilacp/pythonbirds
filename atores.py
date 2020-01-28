54f774dfcd07472d193b0cffefa167120598d00a
ludmilacp <ludmila.canfield@gmail.com>
2020-01-27 22:57:33 -0200

    'implementado o lançamento horizontal. só falta colisao com chao'

--- atores.py
+++ atores.py
@@ -125,6 +125,7 @@ class Passaro(Ator):
         if self.foi_lancado():
             delta_t = tempo - self._tempo_de_lancamento
             self._calcular_posicao_vertical(delta_t)
+            self.calcular_posicao_horizontal(delta_t)
         return super().calcular_posicao(tempo)
 
     def lancar(self, angulo, tempo_de_lancamento):
@@ -145,6 +146,12 @@ class Passaro(Ator):
         y_atual += self.velocidade_escalar * delta_t * math.sin(angulo_radianos)
         y_atual -= (GRAVIDADE * (delta_t ** 2)) / 2
         self.y = y_atual
+
+    def calcular_posicao_horizontal(self, delta_t):
+        x_atual = self._x_inicial
+        angulo_radianos = math.radians(self._angulo_de_lancamento)
+        x_atual += self.velocidade_escalar * delta_t * math.cos(angulo_radianos)
+        self.x = x_atual
 
 
 class PassaroAmarelo(Passaro):
